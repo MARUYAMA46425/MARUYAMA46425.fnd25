03451e03bae938c288a543d91043fbd4235b3805
YUUDAI-MARUYAMA <159223745+MARUYAMA46425@users.noreply.github.com>
2024-03-21 14:03:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,112 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
-function test(actual, expected) {
-  if (JSON.stringify(actual) === JSON.stringify(expected)) {
-    console.log("OK! Test PASSED.");
-  } else {
-    console.error("Test FAILED. Try again!");
-    console.log("    actual: ", actual);
-    console.log("  expected: ", expected);
-    console.trace();
-  }
-}
+// ローカルストレージの設定
+const storage = localStorage;
+console.log(storage.length);
 
 
-const prices = [100, 150, 200];
-
-prices.push(300);
-// const taxOfprace = tax => {
-//   const result = prices.map(price =>{
-//     let x = []
-//     if(price >= 150|| price!==0){
-//       x.push(price);
-//     }
-//     return x * tax;
-//   });
-//   // const result = [];
-//   // for(const price of prices){
-//   //   result.push(price * tax);
-//   console.log(result);
-//   return result;
+// 動画データの複製
+const works = document.querySelector(".container");
+// const work77 = [...works];
+// console.log(work77);
+// for(const item of work77){
+//   console.log(item);}
+//   for(const i of item){
+//     console.log(i);
 // }
 
-// const priceOver150 = [];
 
-// prices.forEach( price =>{
-//   if(price >= 150){
-//     priceOver150.push(price);
-//     console.log(priceOver150);
-//   }return priceOver150;
-// })
+const originDate = document.querySelector(".sns-box");
+const cloned = originDate.cloneNode(true);
+const cloned2 = originDate.cloneNode(true);
 
-const [onion,apple,snack] = prices;
-// prices.pop(1);
-// prices.pop(1);
-// prices.pop(1);
+const iframe = document.querySelector("iframe");
+// works.appendChild(cloned);
 
-console.log(prices);
+// 要素の配列化
+// const listItems = originDate.children;
+// const listItemArray = [...listItems];
 
-console.log(snack);
-console.log(apple);
-const [yy,...others] = prices;
+// for(const item of listItemArray){
+//   console.log(item);
+// }
 
-console.log(others);
 
-const i= [1,2,3,4,5,...others];
-console.log(i);
+// YouTube埋込みデータ読出
+const inputDate = document.querySelector("#content");
+// console.log(inputDate);
 
-// console.log(taxOfprace(1.1));
-// test(taxOfprace(1.1), [110, 165, 220,]);
+// URL抽出
+function searchUrl() {
+  const arrayOfYoutube = inputDate.value.split(" ");
+  for (const array of arrayOfYoutube) {
+    if (String(array).indexOf("src=") !== -1) {
+      const result = String(array);
+      const srcCut1 = result.substring(result.indexOf("=") + 1);
+      const srcCut2 = srcCut1.substring(1, srcCut1.length - 1);
 
-// document.querySelector('button').addEventListener('click', () => {
-//   // console.log('Clicked');
-//   document.querySelector('p').textContent = 'こんにちは';
-//   // document.querySelector('p').textContent = document.querySelector('button').textContent;
-// });
+      // console.log(srcCut2);
+      iframe.setAttribute("src", srcCut2);
+      // console.log(iframe);
+      // storage[storage.length+1] = srcCut1;
+
+      // cloned.setAttribute("iframe",iframe);
+      // console.log(cloned);
+    }
+    // works.appendChild(cloned);
+  }
+  // cloned.appendChild(iframe);
+  works.appendChild(cloned);
+  works.appendChild(cloned2);
+  
+  console.log(works);
+}
+
+// イベントリスナー
+const sendBtn = document.getElementById("sendBtn");
+sendBtn.addEventListener("click", searchUrl);
+
+
+
+const homePic = document.querySelector("#homePic");
+console.log(homePic);
+// homePic.setAttribute("src",);
+
+// 繰り返し処理
+let i = 0;
+let y = "a";
+const intervalAct = function () {
+  console.log("test");
+  console.log(i);
+  if (i >= 60) {clearInterval(timer)};
+  i++;
+  if( y === "a"){
+    console.log(y);
+    y = "b";
+    homePic.setAttribute("src","images/IMG_7207.jpg");
+  }else if (y === "b"){
+    console.log(y);
+    y = "c";
+    homePic.setAttribute("src","images/IMG_7201c.jpeg");
+  }else if(y === "c"){
+    console.log(y);
+    y="d";
+    homePic.setAttribute("src","images/04A177F5-A9DD-4BBD-B693-18AAE89443A9_1_105_c.jpeg");
+  }else if(y === "d"){
+    console.log(y);
+    y="a"
+    homePic.setAttribute("src","images/image.png");
+  }  
+};
+
+const timer = setInterval(intervalAct, 6000);
+
+// 1.text読み込み
+// 2.textからURL抽出
+// 3.youtubeの埋め込み追加
+// 4.埋め込み動画のlinkを2.のURLに書き換え
+// 5.pb操作する度1〜４を繰り返し行う
+// 5.埋め込んだ動画のurlをwebストレージに保管（オブジェクト形式）
+// 6.webサイト更新してもデータ保持
+// 7.不要な動画は削除
